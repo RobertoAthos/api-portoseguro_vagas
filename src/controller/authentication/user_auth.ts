42e878b614ef0d55b6472797f2a545b55a6d8425
Roberto Athos <robertoathos22@gmail.com>
2023-07-22 09:15:24 -0300

    fix: moved updateUser function to the respective file

--- src/controller/authentication/user_auth.ts
+++ src/controller/authentication/user_auth.ts
@@ -59,33 +59,4 @@ export const UserLogin = async (req: Request, res: Response) => {
   }
 };
 
-export const UpdateUser = async (req: Request, res: Response) => {
-  try {
-    const { id } = req.params;
-    const { fullName, email, location, cv, avatar } = req.body;
-    const user = await UserModel.findById(id);
-    if (!user) {
-      res.status(404).send("Usuário não existe");
-    }
 
-    user!.fullName = fullName;
-    user!.email = email;
-    user!.location = location;
-    user!.cv = cv;
-    user!.avatar = avatar;
-
-    if ("cv" in req.files! && Array.isArray(req.files["cv"])) {
-      user!.cv = req.files["cv"][0].path;
-    }
-    
-    if ("avatar" in req.files! && Array.isArray(req.files["avatar"])) {
-      user!.avatar = req.files["avatar"][0].path;
-    }
-
-    const updatedUser = await user!.save();
-
-    res.status(200).json(updatedUser);
-  } catch (error: any) {
-    res.status(400).send(error.message);
-  }
-};
---
+++ src/controller/user_account.ts
@@ -0,0 +1,39 @@
+
+import { Request, Response } from "express";
+import { UserModel } from "../model/user_auth_model";
+
+export const UpdateUser = async (req: Request, res: Response) => {
+    try {
+      const { id } = req.params;
+      const { fullName, email, location, cv, avatar } = req.body;
+      const user = await UserModel.findById(id);
+      if (!user) {
+        res.status(404).send("Usuário não existe");
+      }
+  
+      user!.fullName = fullName;
+      user!.email = email;
+      user!.location = location;
+      user!.cv = cv;
+      user!.avatar = avatar;
+  
+      if ("cv" in req.files! && Array.isArray(req.files["cv"])) {
+        user!.cv = req.files["cv"][0].path;
+      }
+      
+      if ("avatar" in req.files! && Array.isArray(req.files["avatar"])) {
+        user!.avatar = req.files["avatar"][0].path;
+      }
+  
+      const updatedUser = await user!.save();
+  
+      res.status(200).json(updatedUser);
+    } catch (error: any) {
+      res.status(400).send(error.message);
+    }
+  };
+
+
+export const DeleteUser = async (req: Request, res: Response) => {}
+
+export const UpdatePassword = async (req: Request, res: Response) => {}
