5fe1f948c2326b4910c6dc3d01e91929a57d27f8
Roberto Athos <robertoathos22@gmail.com>
2023-07-22 08:06:13 -0300

    feat: added location to user´s schema

--- src/model/user_auth_model.ts
+++ src/model/user_auth_model.ts
@@ -5,8 +5,9 @@ const UserSchema = new mongoose.Schema({
     fullName: {type: String, required:true},
     email: {type: String, required:true},
     password: {type: String, required:true},
-    cv: {type: String, required:true},
-    photo: String,
+    location: {type:String, required:true},
+    cv: String,
+    avatar: String
 })
 
 export const UserModel = mongoose.model('Users', UserSchema)
